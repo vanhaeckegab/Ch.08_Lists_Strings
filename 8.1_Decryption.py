888a8599c75d69d5c52cc20fe37ea4959a44cefb
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-24 19:34:53 -0600

    Create 8.1_Decryption.py

---
+++ 8.1_Decryption.py
@@ -0,0 +1,11 @@
+'''
+An encryption program was used to generate the following secret code. The encryption program converted each character 
+of the string into its ASCII decimal number, applied a +/-20 algorithm to it and then converted it back to
+characters. Your task is to write a Decryption program to decipher the following secret code. Don't waste time changing 
+your program 40 times. Use a FOR loop from -20 to +20 to generate all the possibilities in one run of your program.
+
+Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
+'''
+
+
+
