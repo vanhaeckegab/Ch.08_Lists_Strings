05e1a809553b26d912952e305f42962623af1b06
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-24 20:05:28 -0600

    Create 8.3_Adventure.py

---
+++ 8.3_Adventure.py
@@ -0,0 +1,8 @@
+'''
+ADVENTURE PROGRAM
+-----------------
+1.) Use the pseudo-code on the website to help you set up the basic move through the house program
+2.) Print off a physical map for players to use with your program
+3.) Expand your program to make it a real adventure game
+
+'''
